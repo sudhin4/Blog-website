40a2bb627491f131c25c486b6ce59b0c2e0d9dd6
sudhin4 <sudhinnaveen38@gmail.com>
2024-10-04 23:22:10 +0530

    comment section add

--- assests/script.js
+++ assests/script.js
@@ -5,3 +5,15 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+let commentName = document.getElementById("Name_input");
+let commentSec = document.getElementById("comment_input");
+let btn = document.getElementById("submit_btn");
+function commentsection (){
+    let name = commentName.value = "";
+    let comment = commentSec.value = "";
+    
+}
+btn.addEventListener('click' ,() => {
+    commentsection();
+})
